0e4745f01b743fca1d77e468b5896eb968f99566
tidusjar <tidusjar@gmail.com>
2020-11-19 21:56:48 +0000

    Added some unit tests around the serivce

--- src/ShakespeareanPokemonDescriptor.Services.Tests/PokemonDescriptorServiceTests.cs
+++ src/ShakespeareanPokemonDescriptor.Services.Tests/PokemonDescriptorServiceTests.cs
@@ -1,18 +1,84 @@
+using Moq;
 using NUnit.Framework;
+using ShakespeareanPokemonDescriptor.PokeApi;
+using ShakespeareanPokemonDescriptor.PokeApi.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ShakespeareanPokemonDescriptor.Services.Tests
 {
     public class PokemonDescriptorServiceTests
     {
+        private PokemonDescriptorService Service { get; set; }
+        private Mock<IPokemonApiClient> PokemonApiClientMock { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            PokemonApiClientMock = new Mock<IPokemonApiClient>();
+            Service = new PokemonDescriptorService(PokemonApiClientMock.Object);
         }
 
         [Test]
-        public void Test1()
+        public async Task InvalidPokemonName_Api_Returns_Null()
         {
-            Assert.Pass();
+            var result = await Service.Describe("some silly name", "en", CancellationToken.None);
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+
+        [Test]
+        public async Task InvalidPokemonSpeciesName_Api_Returns_Null()
+        {
+            PokemonApiClientMock.Setup(x => x.SearchPokemon(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(new PokemonSearchResult
+            {
+                Id = 1
+            });
+
+            var result = await Service.Describe("Charmander", "en", CancellationToken.None);
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+
+        [TestCase(null, TestName = "NullRequestLanguage_Should_DefaultTo_En")]
+        [TestCase("", TestName = "EmptyRequestLanguage_Should_DefaultTo_En")]
+        public async Task EmptyRequestLanguage_Should_DefaultTo_En(string input)
+        {
+            SetupBasicDataMocks();
+
+            var result = await Service.Describe("some silly name", input, CancellationToken.None);
+            Assert.That(result, Is.EqualTo("English"));
+        }
+
+
+        private void SetupBasicDataMocks()
+        {
+            PokemonApiClientMock.Setup(x => x.SearchPokemon(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(new PokemonSearchResult
+            {
+                Id = 1
+            });
+
+            PokemonApiClientMock.Setup(x => x.GetSpecies(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(new PokemonSpeciesSearchResult
+            {
+                Results = new List<PokemonSpeciesFlavourResult>
+                {
+                    new PokemonSpeciesFlavourResult
+                    {
+                        Language = new LanguageResult
+                        {
+                            Name = "jp"
+                        },
+                        Text = "NotEnglish"
+                    },
+                    new PokemonSpeciesFlavourResult
+                    {
+                        Language = new LanguageResult
+                        {
+                            Name = "en"
+                        },
+                        Text = "English"
+                    },
+                }
+            });
         }
     }
 }
--- src/ShakespeareanPokemonDescriptor.Services/PokemonDescriptorService.cs
+++ src/ShakespeareanPokemonDescriptor.Services/PokemonDescriptorService.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
+using ShakespeareanPokemonDescriptor.PokeApi;
 
 namespace ShakespeareanPokemonDescriptor.Services
 {
     public class PokemonDescriptorService : IDiscriptorService
     {
+        private readonly IPokemonApiClient _pokemonApiClient;
+
+        public PokemonDescriptorService(IPokemonApiClient pokemonApiClient)
+        {
+            _pokemonApiClient = pokemonApiClient;
+        }
+
         public async Task<string> Describe(string name, string language, CancellationToken cancellationToken)
         {
             return null;
